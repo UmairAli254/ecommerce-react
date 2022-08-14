2b4973dde342c7e2beb5f0b5688467d4414e3e10
Umair Ali <47986782+UmairAli254@users.noreply.github.com>
2022-08-14 15:17:10 +0500

    Navbar Responsive
    Navbar is fully responsive now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import OfferBar from "./components/OfferBar";
+import OfferBar from "./components/OfferBar/OfferBar";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
